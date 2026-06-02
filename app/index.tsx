dea98e931362cf527fc6b7e9ade891762c30936b
Sasen12 <techsavvy356@gmail.com>
2026-06-02 12:09:59 +1000

    Always show landing page at root

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,5 @@
-import { DefaultTabRedirect } from "@/components/navigation/DefaultTabRedirect";
 import { ProductLandingPage } from "@/components/marketing/ProductLandingPage";
-import { useAuthStore } from "@/store/authStore";
 
 export default function IndexRoute() {
-  const user = useAuthStore((state) => state.user);
-  if (user) return <DefaultTabRedirect />;
   return <ProductLandingPage />;
 }
